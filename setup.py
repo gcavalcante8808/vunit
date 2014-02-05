1232ba1c952f37228013aadae2b8d5e8d8cbbdb6
Gabriel Abdalla Cavalcante <gabriel.silva@serpro.gov.br>
2014-02-05 11:26:00 -0200

    Added setup.py dist utils module.

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#__author__ = '01388863189'
+#
+# Done by Gabriel Abdalla Cavalcante Silva at Receita Federal do Brasil,
+#
+# Licensed under the Apache License, Version 2.0, that can be viewed at:
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+
