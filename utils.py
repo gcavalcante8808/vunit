b0a72a14b264ac5e030868372e525350944d710c
Gabriel Abdalla Cavalcante <gabriel.silva@serpro.gov.br>
2014-02-05 09:49:14 -0200

    get dir and write resources functions moved into utils.

--- utils.py
+++ utils.py
@@ -7,3 +7,18 @@
 # Licensed under the Apache License, Version 2.0, that can be viewed at:
 #   http://www.apache.org/licenses/LICENSE-2.0
 """ This module provides usefull functions to the project."""
+import os
+import pickle
+
+
+def get_or_create_dir(directory='resource_lists'):
+    """ Verify if the resource lists dir exists and create it if necessary."""
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
+def write_resource_file(rfile, value):
+    """ Take values and write them into a specified (r)file. """
+    get_or_create_dir()
+    with open('resource_lists'.join(rfile), 'wb') as resource_file:
+        pickle.dump(value, resource_file)
