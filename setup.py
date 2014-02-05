c8589b5634e8321d3f89f9190d0ed4d0e6b44656
Gabriel Abdalla Cavalcante <gabriel.silva@serpro.gov.br>
2014-02-05 11:45:36 -0200

    Updated and Added the vunit_utils.py module.

--- setup.py
+++ setup.py
@@ -14,15 +14,15 @@ except ImportError:
     py2exe = None
 
 setup(name='vunit',
-      version='0.1.0',
+      version='0.1.1',
       scripts=['vunit.py', 'vunit_cfg.py'],
       author='Gabriel Abdalla Cavalcante',
-      data_files=['test_vunit_cfg.py', 'requirements.txt'],
+      data_files=['test_vunit_cfg.py', 'requirements.txt', 'vunit_utils.py'],
       install_requires=['pysphere'],
       author_email='gabriel.cavalcante88@gmail.com',
       description = ("""Vunit is a python script that relies on pysphere library
-      to test the Vmware Esxi and Vcenter environment with some usefull tests. 
-      With this tool, system admins can verify impacts on environments before or 
+      to test the Vmware Esxi and Vcenter environment with some usefull tests.
+      With this tool, system admins can verify impacts on environments before or
       after a maintenance."""),
       url='https://github.com/gcavalcante8808',
       classifiers=[
