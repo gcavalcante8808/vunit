cb3286dd3b17bc77c60f40b7612548e0794ab69a
Gabriel Abdalla Cavalcante <gabriel.silva@serpro.gov.br>
2014-02-07 09:34:57 -0200

    README.md now will be a share/doc/vunit.

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@
 # Licensed under the Apache License, Version 2.0, that can be viewed at:
 #   http://www.apache.org/licenses/LICENSE-2.0
 #
+import os
 from distutils.core import setup
 try:
     import py2exe
@@ -14,16 +15,18 @@ except ImportError:
     py2exe = None
 
 setup(name='vunit',
-      version='0.1.2',
+      version='0.1.3',
       author='Gabriel Abdalla Cavalcante',
       scripts=['vunit/vmwtest.py', 'vunit/vunit_cfg.py'],
       packages=['vunit'],
+      data_files=[('share/doc/vunit', ['README.md']],
       install_requires=['pysphere'],
       author_email='gabriel.cavalcante88@gmail.com',
       description = ("""Vunit is a python package that relies on pysphere
       library to provide tests for the Vmware Esxi and Vcenter environment.
       With this tool, system admins can verify impacts on environments before or
       after a maintenance."""),
+      long_description=read('README.md'),
       url='https://github.com/gcavalcante8808',
       classifiers=[
         'Development Status :: 5 - Production/Stable',
